3a0ee207a520d5ad2e64e49ea10dcda1dd5349a4
TakeruTanaka <takeru.tt5672@gmail.com>
2021-01-25 04:13:13 +0900

    add serial

--- main.go
+++ main.go
@@ -1,8 +1,71 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/takeru56/tcompiler/compiler"
+	"github.com/takeru56/tcompiler/parser"
+	"github.com/takeru56/tcompiler/token"
+)
+
+type SourceJSON struct {
+	SourceCode string `json:"source_code"`
+}
+
+type BytecodeJSON struct {
+	ByteCode string `json:"byte_code"`
+}
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("public")))
+	http.HandleFunc("/compile/", compileHandler)
 	http.ListenAndServe(":3000", nil)
+}
+
+func compileHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "POST":
+		// body := r.Body
+		// defer body.Close()
+
+		// buf := new(bytes.Buffer)
+		// io.Copy(buf, body)
+
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+		var sj SourceJSON
+		json.Unmarshal(body, &sj)
+
+		w.WriteHeader(http.StatusCreated)
+		source := sj.SourceCode
+		fmt.Println(source)
+
+		tok := token.New(source)
+		parser, err := parser.New(tok)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		p, err := parser.Program()
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		c := compiler.Exec(p)
+		outjson, err := json.Marshal(BytecodeJSON{c.Bytecode()})
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(source)
+		fmt.Fprint(w, string(outjson))
+	default:
+		fmt.Fprint(w, "Method not allowed\n")
+	}
 }
--- public/assets/index.js
+++ public/assets/index.js
@@ -1,1 +1,65 @@
-var myCodeMirror = CodeMirror(document.body);
+var myEditor = document.getElementById('myEditor');
+var editor = CodeMirror.fromTextArea(myEditor, {
+  lineNumbers: true,
+  mode: "ruby"
+});
+editor.save();
+
+const connectButton = document.getElementById('connect');
+let port;
+if ('serial' in navigator) {
+  connectButton.addEventListener('click', function () {
+    console.log("click");
+
+    if (port) {
+      port.close();
+      port = undefined;
+      connectButton.innerText = 'Connect';
+    }
+    else {
+      getReader();
+    }
+  });
+
+  connectButton.disabled = false;
+}
+
+const runButton = document.getElementById('run');
+const outputArea = document.getElementById('output');
+
+// refs: https://wicg.github.io/serial/#dom-serialportinfo
+async function getReader() {
+  port = await navigator.serial.requestPort({});
+  await port.open({ baudRate: 115200 });
+  connectButton.innerText = '🔌 Disconnect';
+
+  runButton.addEventListener('click', (event) => {
+    const code = editor.getValue();
+    outputArea.innerText = 'Compiling...'
+    $.ajax({
+      type:     "POST",
+      url:      "http://localhost:3000/compile/",
+      data: JSON.stringify({
+        "source_code": code
+      }),
+      dataType: "json",
+      xhrFields: {
+          withCredentials: true
+      },
+
+      }).done(function(data) {
+        const byte_code = data["byte_code"]
+        if (port && port.writable) {
+          bytes = new Uint8Array(byte_code.match(/.{1,2}/g).map(v => parseInt(v, 16)))
+          const writer = port.writable.getWriter();
+          console.log(byte_code)
+          const encoder = new TextEncoder();
+          writer.write(encoder.encode(byte_code));
+          writer.releaseLock();
+        }
+
+      }).fail(function(data) {
+          console.log('Ajax fail (communication error)');
+      });
+  });
+}
